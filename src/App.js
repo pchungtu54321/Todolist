2c95f11a471a4724d420bc73b55c4937ad23d7c7
unknown <pchungtu@gmail.com>
2023-04-22 02:14:18 +0700

    First

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
 import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
+
 function App() {
+
+  const[userList, setUserList] = useState([])
+  const[selectedUser, setSelectedUser] = useState('')
+  const[todoList, setTodoList] = useState([])
+
+  //userList
+  useEffect(() => {
+    async function fetchUsers() {
+      const response = await fetch ('https://jsonplaceholder.typicode.com/users');
+      const data = await response.json();
+      setUserList(data);
+    }
+    console.log(userList)
+    fetchUsers();
+  }, []);
+
+  //todoList
+  useEffect(() => {
+    async function fetchTodos() {
+      if(selectedUser) {
+        const response = await fetch (`https://jsonplaceholder.typicode.com/users/${selectedUser}/todos`);
+        const data = await response.json();
+        setTodoList(data);
+      }
+    }
+    console.log(todoList)
+    fetchTodos();
+  }, [selectedUser]);
+
+  //selectedUser
+  function handleSelectUser(event) {
+    setSelectedUser(event.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <span>User</span>
+        <div>
+          <select value = {selectedUser} onChange = {handleSelectUser}>
+            <option value =""> Select a user</option>
+            {
+              userList.map((user) =>(
+              <option key = {user.id} value = {user.id}> {user.username}</option>
+            ))}
+          </select>
+        
+        </div>
+      </div>
+      <div>
+        <span>Taks</span>
+        <div>
+          {
+            todoList.map((todo) => (
+            <div key={todo.id}>
+              <h3>{todo.title}</h3>
+              <p>{todo.completed ? "Completed" : "Not completed"}</p>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
